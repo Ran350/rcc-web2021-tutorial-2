f9cfccab24fba06a42e68626b9cf8293921bf235
Hoshi Ran <ureshinostyle350@gmail.com>
2021-07-07 01:09:14 +0900

    3. カウンター用の状態変数を用意しよう

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+let count = 0;
+
+app.get("/count/increment", (req, res) => {
+  count += 1;
+  res.send({ count });
+});
+
+app.get("/count/decrement", (req, res) => {
+  count -= 1;
+  res.send({ count });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
