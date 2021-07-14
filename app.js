48ff08404c7c9f2782f30a6dac5ff6e67c40ded3
Hoshi Ran <ureshinostyle350@gmail.com>
2021-07-14 17:04:04 +0900

    5. フロントエンドチュートリアル用の変更

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 
+const cors = require("cors");
+app.use(cors());
+
+const port = 4000;
+
+let count = 0;
+
+// --- ルートページ ---
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-let count = 0;
-
-app.get("/count/increment", (req, res) => {
-  count += 1;
-  res.send({ count });
-});
-
-app.get("/count/decrement", (req, res) => {
-  count -= 1;
-  res.send({ count });
-});
-
+// --- Multi Page Application ---
 app.get("/counter", (req, res) => {
   res.render("counter.ejs", { count });
 });
@@ -32,6 +28,27 @@ app.post("/counter/decrement", (req, res) => {
   res.render("counter.ejs", { count });
 });
 
+// --- API ---
+app.get("/count", (req, res) => {
+  res.send({ count });
+});
+
+app.post("/count/increment", (req, res) => {
+  count += 1;
+  res.send({ count });
+});
+
+app.post("/count/decrement", (req, res) => {
+  count -= 1;
+  res.send({ count });
+});
+
+// --- 404ページ ---
+app.get("*", (req, res) => {
+  res.status(404).send(`Page is not found`);
+});
+
+// --- 接続を待ち受けるときの処理 ---
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
